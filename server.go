33a83dc2c8c49ea32db15383d76e8c729e5d3e6a
wings <hotman8168@gmail.com>
2017-10-31 23:16:30 +0800

    ECHO PARAM

--- server.go
+++ server.go
@@ -15,6 +15,9 @@ type User struct {
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
