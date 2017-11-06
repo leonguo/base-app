121289656532b6b2a77a7260fb8fd7640a9ecaab
wings <hotman8168@gmail.com>
2017-11-06 23:36:05 +0800

    IRIS route

--- iris.go
+++ iris.go
@@ -7,14 +7,6 @@ import (
 	"echo-app/api/controllers"
 )
 
-// Movie is our sample data structure.
-type Movie struct {
-	Name   string `json:"name"`
-	Year   int    `json:"year"`
-	Genre  string `json:"genre"`
-	Poster string `json:"poster"`
-}
-
 func main() {
 	app := iris.New()
 	app.Use(recover.New())
@@ -27,13 +19,40 @@ func main() {
 	app.Get("ping", func(ctx iris.Context) {
 		ctx.WriteString("pong")
 	})
-
-	app.Get("test", func(ctx iris.Context) {
-		ctx.JSON(iris.Map{"message":"hello iris"})
-	})
+	authMiddleware := func(ctx iris.Context) {
+		ctx.Next()
+	}
+	v1 := app.Party("/v1", authMiddleware)
+	usersAPI := v1.Party("/users")
+	{
+		// http://localhost:8080/api/users
+		usersAPI.Get("/", h)
+		usersAPI.Post("/", h)
+		// http://localhost:8080/api/users/42
+		usersAPI.Get("/{userid:int}", func(ctx iris.Context) {
+			ctx.Writef("user with id: %s", ctx.Params().Get("userid"))
+		})
+	}
 	app.Run(iris.Addr("localhost:8080"))
 }
 
 func notFoundHandler(ctx iris.Context) {
 	ctx.JSON(iris.Map{"message":"not found"})
+}
+
+func h(ctx iris.Context) {
+	method := ctx.Method()       // the http method requested a server's resource.
+	subdomain := ctx.Subdomain() // the subdomain, if any.
+
+	// the request path (without scheme and host).
+	path := ctx.Path()
+	// how to get all parameters, if we don't know
+	// the names:
+	paramsLen := ctx.Params().Len()
+
+	ctx.Params().Visit(func(name string, value string) {
+		ctx.Writef("%s = %s\n", name, value)
+	})
+	ctx.Writef("Info\n\n")
+	ctx.Writef("Method: %s\nSubdomain: %s\nPath: %s\nParameters length: %d", method, subdomain, path, paramsLen)
 }
